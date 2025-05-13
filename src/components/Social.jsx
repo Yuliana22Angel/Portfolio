13d4aa69acd2ef06c8d4e2589f42cd60311cb0b1
DESKTOP-HH01H4H\maria <angelica12infante@gmail.com>
2025-05-13 15:05:07 -0400

    cambios en la api

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const socialShare = [
   {
     img: "whatsapp",
-    link: "https://wa.me/584125838229",
+    link: "https://api.whatsapp.com/send?phone=584125838229",
   },
   {
     img: "github",
