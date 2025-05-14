c6c86af619c1305061ad90affcc2d2e6519471e2
DESKTOP-HH01H4H\maria <angelica12infante@gmail.com>
2025-05-13 20:35:10 -0400

    cambios en el css de las sombras

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
 const socialShare = [
   {
     img: "whatsapp",
-    link: "https://api.whatsapp.com/send?phone=584125838229",
+    link: "https://wa.me/584125838229?text=Hola%2C%20quiero%20más%20información",
+    mobileLink: "whatsapp://send?phone=584125838229&text=Hola%2C%20quiero%20más%20información",
   },
   {
     img: "github",
     link: "https://github.com/Yuliana22Angel",
   },
 ];
+
+const handleClick = (e, item) => {
+  if (item.img === "whatsapp" && isMobile) {
+    e.preventDefault(); // Previene navegación normal
+    window.location.href = item.mobileLink; // Fuerza apertura en app
+  }
+};
+
 const Social = () => {
   return (
     <ul>
       {socialShare.map((item, i) => (
         <li key={i}>
-          <a href={item.link} target="_blank" rel="noopener noreferrer">
+          <a
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={(e) => handleClick(e, item)}
+          >
             <img
               className="svg2"
               src={`img/svg/social/${item.img}.svg`}
@@ -29,3 +45,4 @@ const Social = () => {
 };
 
 export default Social;
+
